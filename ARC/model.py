66b324d8a6ed776fe044c018ba21da1683d0637f
ostwind <lihan.yao@gmail.com>
2018-08-05 19:24:31 -0400

    adding model and train

--- ARC/model.py
+++ ARC/model.py
@@ -1,81 +1,124 @@
 from ARC import * 
-
-# meta_data["training_loss"], meta_data["validation_loss"] = [], []
-# meta_data["validation_accuracy"] = []
+import  matplotlib.pyplot as plt 
 
 class ARC(nn.Module):
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, CauchyKerSize, hidden_size, num_glimpses  ):
         super(ARC, self).__init__()
+        self.img_size = 32
         self.batch_size = batch_size
+        self.cauchy_ker_size = CauchyKerSize
+        self.num_glimpses = num_glimpses
+        self.hidden_size = hidden_size
 
-        # ARC implementation: outchannels = 16, kernel_size = (3,3), stride = 1  
-        self.channels = 4
-        self.convolve = nn.Conv2d(1, self.channels, kernel_size = (3,3), stride = 1 ) 
-        self.relu = nn.ReLU()
+        # grids for computing cauchy kernels (attenttion) 
+        self.cauchy_kernel_grid = torch.arange( self.cauchy_ker_size, dtype = torch.float ) - ( self.cauchy_ker_size / 2 + 0.5 )
+        self.filterbank_grid = torch.arange(self.img_size, dtype = torch.float)[None, None, :].expand(
+            self.batch_size, self.cauchy_ker_size, -1)
         
-        self.hidden_size = 576 #35*35*2
-        self.controller = nn.GRU(self.hidden_size, self.hidden_size, batch_first = True) 
+        # ARC implementation: outchannels = 16, kernel_size = (3,3), stride = 1  
+        self.channels = 16
+        self.convolve = nn.Conv2d(1, self.channels, 
+        padding = (1,1), 
+        kernel_size = (3,3), stride = 1 ) 
+        
+        self.attn_patch_size = self.channels * self.cauchy_ker_size * self.cauchy_ker_size
+        self.controller = nn.GRUCell(
+            input_size=self.attn_patch_size, hidden_size= self.hidden_size
+        ) 
 
         # W_g in the paper, projects hidden state of controller to (x_hat, y_hat, delta_hat)
         self.hidden_projection = nn.Linear(self.hidden_size, 3)
-            
-        self.linear_output = nn.Linear(self.hidden_size, 2)
-    
-    def _compute_filterbanks(self, z, delta, gamma, S = 30, N = 12):
-        mu_Z = np.array(  [ z + (i - (((N+1)/2) ) )*delta for i in range(N)   ] )
-        F_Z = []
-        for pixel_index in range(S):
-            F_Z.append( 1+( (pixel_index - mu_Z)/gamma )**2 )
 
-        F_Z = 1/( np.pi*gamma*np.array(F_Z) )
-        F_Z = F_Z.T
+        # maps the final hidden state to raw similarity scores 
+        self.linear_output = nn.Linear(self.hidden_size, 64)
+        self.linear_output2 = nn.Linear(64, 1)
+
+    def _compute_filterbanks(self, z, img_axis_center, gamma):
+        ''' 
+        Attention parameters are computed with processed info from current hidden state h_t and the image
+        Args:
+            z (batch size, 1): Cauchy kernel center 
+            img_axis_center (batch size, ):
+            gamma (B, 1):
+        '''
+        mu_Z = z + img_axis_center #(64, 1) (64, 4, 1)
         
-        F_Z = torch.from_numpy(F_Z)
+        F_Z = self.filterbank_grid - mu_Z[:,:,None]
+        F_Z = 1.0 + (( F_Z / gamma[:, None, None] )**2)
+        F_Z = 1.0 /( np.pi* F_Z * gamma[:, None, None] )
+        
+        F_Z = F_Z/(torch.sum(F_Z, dim = 2) + 1e-4)[:, :, None]
+        F_Z = F_Z.unsqueeze(1).expand(-1, self.channels, -1, -1)
+        
         return F_Z
 
-    def _attend(self, img, attend_params, S = 30, N = 12):
-        x_hat, y_hat, delta_hat = attend_params[:, 0], attend_params[:, 1], attend_params[:, 2]
-
-        x, y = (S - 1)*(x_hat+1)/2, (S - 1)*(y_hat+1)/2 
-        delta = S * (1-np.abs(delta_hat)) / N
-        gamma = np.exp( 1-(2*np.abs(delta_hat) ) )
-
-        F_X, F_Y = self._compute_filterbanks(x, delta, gamma), self._compute_filterbanks(y, delta, gamma)
-        F_Y = torch.transpose(F_Y, 1, 2)
-
-        attn_patch = torch.zeros( 8, self.channels, N, N)
-        for c in range(self.channels):
-            attn_patch[:, c, :,: ] = torch.bmm( torch.bmm(F_X, img[:, c, :, :]), F_Y) 
-                
-        return attn_patch #torch.from_numpy( attn_patch)
-
-    def glimpse(self, img, hidden):
-        attn_params = self.hidden_projection(hidden)
-        attn_params = attn_params.detach().squeeze(0) 
-        #print(attn_params  )
-
-        attn_patch = self._attend(img, attn_params)
-        attn_patch = attn_patch.reshape(attn_patch.shape[0], 1 , -1)
-        _, hidden = self.controller( attn_patch, hidden )
-        return  hidden
-
-    def _conv(self, img):
-        img = self.relu( self.convolve(img) )
-        return img
-
-    def forward(self, support, test):
-        support = self._conv(support)
-        test = self._conv(test)
+    def batched_dot(self, A, B):
+        ''' 
+            Computes a tensor C that's quinessentially the high dimensional dot product
+            along a common axis between A and B  
+        Args:
+            A (Batch size, Channels, I, J)
+            B (Batch size, Channels, J, K)
+        '''
+        A, B = A.unsqueeze(4), B.unsqueeze(2)
+        C = A * B
+        return C.sum(3)
+    
+    def _attend(self, img, attend_params):
+        # computation found in section 2.1
+        x_hat, y_hat  = attend_params[:, 0][:, None], attend_params[:, 1][:, None]
+        delta_hat = torch.abs( attend_params[:, 2] )
         
-        hidden = torch.zeros(1, self.batch_size, self.hidden_size)
+        x, y = (self.img_size - 1)*(x_hat+1)/2.0, (self.img_size - 1)*(y_hat+1)/2.0 
+        delta = self.img_size * (1.0 - delta_hat) / self.cauchy_ker_size
+        gamma = torch.exp( 1-( 2.0 * delta_hat ) )
+        
+        img_axis_center = delta[:,None] * self.cauchy_kernel_grid[None,:]  # elementwise mult w/ delta
+        
+        F_X = self._compute_filterbanks(x, img_axis_center, gamma) 
+        F_Y = self._compute_filterbanks(y, img_axis_center, gamma)
+        
+        #print(F_Y.shape, img.shape, F_X.shape, attn_patch.shape)
+        #[batch, channel, N, S] [batch, channel, S, S] [batch, channel, S, N] 
+        
+        # Attend to image: Apply Cauchy kernel to the current image to extract a patch 
+        attn_patch = self.batched_dot( F_Y, img)
+        attn_patch = self.batched_dot(attn_patch , torch.transpose(F_X, 2, 3) )
+        
+        return attn_patch
 
-        for i in range(6): # num glimpses
-            if i % 2 == 0:
-                hidden = self.glimpse( support, hidden )
+    def glimpse(self, img, hidden):        
+        # compute  attention parameters
+        attn_params = torch.tanh( self.hidden_projection( hidden ) )
+        
+        # extract relevant patch of the drawing
+        attn_patch = self._attend(img, attn_params)
+        attn_patch = attn_patch.reshape(self.batch_size, -1)
+        
+        # update the RNN hidden state h_t
+        hidden = self.controller( attn_patch, hidden )
+        return hidden
 
-            else: 
-                hidden = self.glimpse( test, hidden )
+    def forward(self, X, ind = None):        
+        # apply convolution to both support and test drawings, adds depth dimension
+        support = F.elu( self.convolve( X[:, 0, :, :].unsqueeze(1) ) )
+        test = F.elu( self.convolve( X[:, 1, :, :].unsqueeze(1) ) )
+        
+        # init RNN (controller)
+        hidden = torch.zeros(self.batch_size, self.hidden_size)
+        
+        glimpse_stats = np.zeros( (self.batch_size, 8) )
+        for i in range(8): # times glimpsing support-test pairs
+            hidden = self.glimpse( support, hidden )
+            hidden = self.glimpse( test, hidden )
 
-        hidden = hidden.squeeze(0)#reshape(4, -1)
-        hidden = self.linear_output( hidden )
+            # compute glimpse score for analysis
+            glimpse_score = F.elu( self.linear_output2( self.linear_output( hidden ) ) ) 
+            glimpse_stats[:, i] = glimpse_score.reshape(-1).detach().numpy()
+
+        # after glimpsing both images, create a raw score for similarity
+        hidden = F.elu( self.linear_output2( self.linear_output( hidden ) ) )
+        
+        if ind:
+            np.save('./data/glimpse_stats/%s' %ind, glimpse_stats)
         return hidden 
--- ARC/train.py
+++ ARC/train.py
@@ -1,55 +1,103 @@
 import os
+import argparse
 import sys 
 sys.path.append(os.getcwd())
 
 from model import *
-#from util.preprocess import paths 
 from ARC import * 
+from batcher import Batcher
 
-def paths(cur_dir):
-    return [os.path.join(cur_dir, o) for o in os.listdir(cur_dir) 
-                            if os.path.isdir(os.path.join(cur_dir,o))]
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=60, help='input batch size')
+parser.add_argument('--CauchyKer_size', type=int, default=4, help='the height / width of glimpse seen by ARC')
+parser.add_argument('--hidden_size', type=int, default=128, help='number of hidden states in ARC controller')
+parser.add_argument('--num_glimpses', type=int, default=8, help='the number glimpses of each image in pair seen by ARC')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+#parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--name', default=None, help='name for checkpoints of this model, R/W directed here')
+arg = parser.parse_args()
 
+model = ARC(batch_size = arg.batch_size, CauchyKerSize = arg.CauchyKer_size,
+hidden_size = arg.hidden_size, num_glimpses = arg.num_glimpses)
+print('running on %s devices' %( torch.cuda.device_count() ) )
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    model.cuda()
 
-Alphabets = paths('./data/train')
-
-
-#AlphabetPath = './data/train/Latin/'
-
-
-
-batch_size = 8
-SupportSet = Alphabet(Alphabets)
-SupportLoader = DataLoader(SupportSet, batch_size= batch_size, shuffle=True, num_workers=0)
-
-TestSet = 0
-#TestSet = Alphabet(Alphabets)   #, constrain_test_symbol= True)
-#TestLoader = DataLoader(TestSet, batch_size=2, shuffle=True, num_workers=1)
-#print(len(TestSet) ) 
-
-model = ARC(batch_size = batch_size)
-metric = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+criterion = nn.BCEWithLogitsLoss() #nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = arg.lr)
 stats = dict()
 stats['loss'] = []
 
-def train(SupportLoader, TestSet):
-    for i in range(3):
-        for ind, (samples1, samples2) in  enumerate(SupportLoader):
-            test, support, label = GetXY(samples1 , samples2)
-            pred = model(support, test)
+loader = Batcher(batch_size= arg.batch_size)
+
+if arg.name is not None:
+    model_name = './'+arg.name + '.pth'
+else: 
+    model_name = './%s_%s.pth' %(arg.glimpse_size, arg.lr)
+
+if os.path.exists(model_name):
+    model.load_state_dict(torch.load( model_name ))
+
+def accuracy(pred, target) -> int:
+    hard_pred = (pred > 0.5).int()
+    correct = (hard_pred == target).sum().item()
+    accuracy = float(correct) / target.size()[0]
+    accuracy = int(accuracy * 100)
+    return accuracy
+
+def one_shot_eval(pred, truth):
+    corrects, error_margin = 0, 0
+    if   truth[ pred[:20].max(0)[1] ] == 1:
+        corrects += 1
+    if   truth[ pred[20:40].max(0)[1]+20 ] == 1:
+        corrects += 1
+    if   truth[ pred[40:].max(0)[1]+40 ] == 1 :
+        corrects += 1
+    return corrects 
+
+def train(loader):
+    model.train()
+    for ind in range(1000*1000):
+        X, Y = loader.fetch_batch("train")
+        if use_cuda:
+            X, Y = X.cuda(), Y.cuda()
+        pred = model(X)
+        loss = criterion(pred, Y.float())
+        stats['loss'].append(loss)
+        
+        if ind % 2000 == 0:
+            print( 'training loss: %.3f   similarity train acc: %d    epoch:  %s' %(
+            np.sum( stats['loss'] )/len(stats['loss']),
+            accuracy(pred, Y), 
+            ind ) )
+            stats['loss'] = []
             
-            loss = metric(pred, label)
-            stats['loss'].append(loss)
-            if ind % 100 == 0:
-                print( '%.4f        %s ' %( np.sum(stats['loss'])/100,
-                torch.sum(label).item() ) )
-                stats['loss'] = []
+            with torch.no_grad():
+                one_shot_by_alphabet = []
+                for alphabet in range(20):
+                    eval_acc = 0 
+                    for test_ind in range(100):
+                        # remove 'part' argument to randomly choose alphabet
+                        X_val, Y_val = loader.fetch_batch()#part = alphabet)
+                        if use_cuda:
+                            X_val, Y_val = X_val.cuda(), Y_val.cuda()
+            
+                        pred_val = model(X_val, test_ind)
+                        eval_acc += one_shot_eval(pred_val, Y_val )
+                    
+                    one_shot_by_alphabet.append(  round(eval_acc/300,2) )
+                    print('Alphabet %s accuracy: %.3f' %(alphabet+30, eval_acc/300))
+            
+            print(one_shot_by_alphabet)
+            print('\n')
+            #exit()
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            if ind % 2000 == 0:
+                torch.save(model.state_dict(), model_name )
 
-            #break
-
-train(SupportLoader, TestSet)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+train(loader)
